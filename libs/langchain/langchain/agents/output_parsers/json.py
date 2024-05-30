c8769523e38611d840fe7ff3a3c66a7f85953173
WangYuxuan93 <hitalexwang@gmail.com>
2024-05-30 16:30:08 +0800

    update json output parser for structured agent

--- libs/langchain/langchain/agents/output_parsers/json.py
+++ libs/langchain/langchain/agents/output_parsers/json.py
@@ -11,6 +11,27 @@ from langchain.agents.agent import AgentOutputParser
 
 logger = logging.getLogger(__name__)
 
+import re
+import json
+
+def parse_all_json(text):
+    pattern = re.compile(r'{\s*"action":\s*"[^"]+",\s*"action_input":\s*(?:\{.*?\}|\s*".*?"\s*)\s*}', re.DOTALL)
+
+    matches = pattern.findall(text)
+
+    # 替换 True 和 False 为 true 和 false
+    def replace_boolean_values(json_str):
+        json_str = json_str.replace(' True', ' true')
+        json_str = json_str.replace(' False', ' false')
+        return json_str
+
+    # 处理所有匹配的部分
+    json_scripts = []
+    for match in matches:
+        valid_json_str = replace_boolean_values(match)
+        json_scripts.append(json.loads(valid_json_str))
+
+    return json_scripts
 
 class JSONAgentOutputParser(AgentOutputParser):
     """Parses tool invocations and final answers in JSON format.
@@ -42,16 +63,20 @@ class JSONAgentOutputParser(AgentOutputParser):
 
     def parse(self, text: str) -> Union[AgentAction, AgentFinish]:
         try:
-            response = parse_json_markdown(text)
+            #print ("json parser origin input:\n",text)
+            response = parse_json_markdown(text, parser=parse_all_json)
+            #print ("json parser response:\n",response)
+            #exit()
             if isinstance(response, list):
                 # gpt turbo frequently ignores the directive to emit a single action
-                logger.warning("Got multiple action responses: %s", response)
-                response = response[0]
-            if response["action"] == "Final Answer":
-                return AgentFinish({"output": response["action_input"]}, text)
+                #logger.warning("Got multiple action responses: %s", response)
+                first_response = response[0]
+                #print ("json parser multi input\n:",response)
+            if response[-1]["action"] == "Final Answer":
+                return AgentFinish({"output": response}, text)
             else:
                 return AgentAction(
-                    response["action"], response.get("action_input", {}), text
+                    first_response["action"], first_response.get("action_input", {}), text
                 )
         except Exception as e:
             raise OutputParserException(f"Could not parse LLM output: {text}") from e
